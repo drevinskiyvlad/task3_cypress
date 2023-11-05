b427042ab6b732bfc2272feebf4e7df23960e0f9
Drevinskyi Vladislav <drevinskiy.vlad@gmail.com>
2023-11-05 22:53:58 +0200

    added reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,21 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '4mswiq',
+  reporter: 'cypress-mochawesome-reporter',
+  video: false,
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress Inline Reporter',
+    embeddedScreenshots: true,
+    inlineAssets: true, //Adds the asserts inline
+
+  },
+
   e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+
     baseUrl: 'https://demo.realworld.io/',
     defaultCommandTimeout: 10000,
   },
